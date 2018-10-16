f1077bb7b4a2da24dbd09308eb694e402579f8e8
olegush <43958767+olegush@users.noreply.github.com>
2018-10-16 19:04:59 +0300

    separated functions

--- password_strength.py
+++ password_strength.py
@@ -1,6 +1,32 @@
+def get_password_char_strength(password):
+    strength_lower = 0
+    strength_upper = 0
+    strength_digit = 0
+    strength_specials = 0
+    for char in list(password):
+        if char.islower():
+            strength_lower = 1
+        if char.isupper():
+            strength_upper = 2
+        if char.isdigit():
+            strength_digit = 2
+        if '!@#$%^&*()_+|<>?~`'.find(char) > -1:
+            strength_specials = 4
+    return strength_upper + strength_digit + strength_lower + strength_specials
 
 
-def get_password_strength(password):
+def get_password_strength(password, password_blacklist):
+    if password_blacklist.count(password) > 0:
+        return 1
+    elif len(password) < 4:
+        return 1
+    else:
+        strength_len = 1
+        return strength_len + get_password_char_strength(password)
+
+
+if __name__ == '__main__':
+    user_password = raw_input('Password:')
     password_blacklist = ['Password1',
                           'Welcome1',
                           'P @ ssword',
@@ -10,31 +36,5 @@ def get_password_strength(password):
                           'Hello123',
                           'Welcome123',
                           'P @ ssword1']
-    if password_blacklist.count(password) > 0:
-        return 1
-    elif len(password) < 4:
-        return 1
-    else:
-        strength_len = 1
-        strength_lower = 0
-        strength_upper = 0
-        strength_digit = 0
-        strength_specials = 0
-        for char in list(password):
-            if char.islower():
-                strength_lower = 1
-            if char.isupper():
-                strength_upper = 2
-            if char.isdigit():
-                strength_digit = 2
-            if '!@#$%^&*()_+|<>?~`'.find(char) > -1:
-                strength_specials = 4
-        strength = strength_len + strength_upper + strength_digit \
-            + strength_lower + strength_specials
-        return strength
-
-
-if __name__ == '__main__':
-    user_password = raw_input('Password:')
     print('Your password\'s strength:{}'
-          .format(get_password_strength(user_password)))
+          .format(get_password_strength(user_password, password_blacklist)))
