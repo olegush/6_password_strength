a392a5a7eded3a31e3d2365e30ba09fe0dc8f1d6
olegush <43958767+olegush@users.noreply.github.com>
2018-10-17 08:40:02 +0300

    bugs are fixed, code improved

--- password_strength.py
+++ password_strength.py
@@ -1,40 +1,49 @@
+import getpass
+import os.path
+import sys
+import string
+
+
+def load_data(filepath):
+    with open(filepath, 'rb') as file:
+        return file.read()
+
+
 def get_password_char_strength(password):
     strength_lower = 0
     strength_upper = 0
     strength_digit = 0
     strength_specials = 0
-    for char in list(password):
+    for char in password:
         if char.islower():
             strength_lower = 1
         if char.isupper():
             strength_upper = 2
         if char.isdigit():
             strength_digit = 2
-        if '!@#$%^&*()_+|<>?~`'.find(char) > -1:
+        if string.punctuation.find(char) > -1:
             strength_specials = 4
     return strength_upper + strength_digit + strength_lower + strength_specials
 
 
 def get_password_strength(password, password_blacklist):
+    min_password_length = 4
     if password_blacklist.count(password) > 0:
-        return 1
-    elif len(password) < 4:
-        return 1
+        return '1 (the password in blacklist)'
+    elif len(password) < min_password_length:
+        return '1 (the password less than 4 symbols)'
     else:
         strength_len = 1
         return strength_len + get_password_char_strength(password)
 
 
 if __name__ == '__main__':
-    user_password = raw_input('Password:')
-    password_blacklist = ['Password1',
-                          'Welcome1',
-                          'P @ ssword',
-                          'Summer1!',
-                          'password',
-                          'Fa$hion1',
-                          'Hello123',
-                          'Welcome123',
-                          'P @ ssword1']
-    print('Your password\'s strength:{}'
+    if len(sys.argv) < 2:
+        exit('need filename')
+    user_filepath = sys.argv[1]
+    if not os.path.exists(user_filepath):
+        exit('need correct file path')
+    user_password = getpass.getpass(prompt='Password: ', stream=None)
+    password_blacklist = load_data(user_filepath)
+    print('Your password strength is:{}'
           .format(get_password_strength(user_password, password_blacklist)))
